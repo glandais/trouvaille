d5852e37fe5d6e5bed41ff74ce6f58a4d675a714
Gabriel Landais <gabriel.landais@gmail.com>
2025-06-25 21:36:11 +0200

    Save/update

---
+++ trouvaille-back/src/main/java/io/github/glandais/trouvaille/service/AnnonceMapper.java
@@ -0,0 +1,26 @@
+package io.github.glandais.trouvaille.service;
+
+import io.github.glandais.trouvaille.entity.AnnonceEntity;
+import io.github.glandais.trouvaille.openapi.beans.AnnonceCreate;
+import io.github.glandais.trouvaille.openapi.beans.AnnonceUpdate;
+import org.bson.types.ObjectId;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "cdi",
+        unmappedSourcePolicy = ReportingPolicy.ERROR)
+public abstract class AnnonceMapper {
+
+    protected ObjectId mapObjectId(String objectId) {
+        return new ObjectId(objectId);
+    }
+
+    @Mapping(source = "photosIds", target = "photos")
+    public abstract AnnonceEntity mapAnnonceCreate(AnnonceCreate data);
+
+    @Mapping(source = "photosIds", target = "photos")
+    public abstract void updateAnnonceEntity(@MappingTarget AnnonceEntity annonceEntity, AnnonceUpdate data);
+
+}
--- trouvaille-back/src/main/java/io/github/glandais/trouvaille/service/AnnonceService.java
+++ trouvaille-back/src/main/java/io/github/glandais/trouvaille/service/AnnonceService.java
@@ -1,6 +1,7 @@
 package io.github.glandais.trouvaille.service;
 
 import io.github.glandais.trouvaille.entity.AnnonceEntity;
+import io.github.glandais.trouvaille.entity.AnnonceEntityStatut;
 import io.github.glandais.trouvaille.openapi.beans.*;
 import io.github.glandais.trouvaille.repository.AnnonceRepository;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -11,6 +12,7 @@ import org.bson.types.ObjectId;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Date;
 
 @ApplicationScoped
 @RequiredArgsConstructor
@@ -20,6 +22,8 @@ public class AnnonceService {
 
     final AnnonceEntityMapper annonceEntityMapper;
 
+    final AnnonceMapper annonceMapper;
+
     final UserService userService;
 
     public Annonces listAnnonces(AnnonceType type, AnnonceNature nature, BigInteger page, BigInteger limit, String search, String userPseudo, BigDecimal prixMin, BigDecimal prixMax, Double latitude, Double longitude, BigDecimal distanceMax, String sortBy, String sortOrder) {
@@ -27,7 +31,14 @@ public class AnnonceService {
     }
 
     public Annonce createAnnonce(AnnonceCreate data) {
-        return new Annonce();
+        AnnonceEntity annonceEntity = annonceMapper.mapAnnonceCreate(data);
+        annonceEntity.setId(new ObjectId());
+        annonceEntity.setUtilisateur(userService.getCurrentUser().getId());
+        annonceEntity.setStatut(AnnonceEntityStatut.active);
+        annonceEntity.setDateCreation(new Date());
+        annonceEntity.setDateModification(new Date());
+        annonceRepository.persist(annonceEntity);
+        return getAnnonce(annonceEntity.getId().toString());
     }
 
     public Annonce getAnnonce(String id) {
@@ -39,6 +50,9 @@ public class AnnonceService {
     public Annonce putAnnonce(String id, AnnonceUpdate data) {
         AnnonceEntity annonceEntity = annonceRepository.findById(new ObjectId(id));
         checkAnnonceOwnership(annonceEntity);
+        annonceMapper.updateAnnonceEntity(annonceEntity, data);
+        annonceEntity.setDateModification(new Date());
+        annonceRepository.update(annonceEntity);
         return getAnnonce(id);
     }
 
