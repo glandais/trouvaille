8ed9548aa21a46572c1ba7b50614cd1a8606e538
Gabriel Landais <gabriel.landais@gmail.com>
2025-07-08 00:17:54 +0200

    fix: fix log

--- trouvaille-back/src/main/java/io/github/glandais/trouvaille/service/AuthService.java
+++ trouvaille-back/src/main/java/io/github/glandais/trouvaille/service/AuthService.java
@@ -63,7 +63,7 @@ public class AuthService {
       return response;
 
     } catch (Exception e) {
-      log.error("Failed to exchange OAuth token with Mattermost", e);
+      log.error("Failed to exchange OAuth token", e);
       throw new RuntimeException("OAuth token exchange failed", e);
     }
   }
